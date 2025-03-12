4de203d0ccd31e2885406b526de1c8101d29403a
nilavanan25m <nilavanan@medscribe.in>
2025-03-12 08:22:24 +0530

    ver 2

---
+++ __/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBUQGQTfsoA2elSMeKfTakX1Pk0NPh5jhY",
+  "authDomain": "portfilo-nila.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "745272951796",
+  "projectId": "portfilo-nila",
+  "storageBucket": "portfilo-nila.appspot.com"
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".t",{
+    strings:["&nbsp; Full Stack Developer","&nbsp; Data Engineer","&nbsp; Gen AI Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
